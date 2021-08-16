db748e2a2df455bd86110ac522e2547b3187bdb9
yeezao <liangyuzhao1999@gmail.com>
2021-08-16 22:30:35 +0800

    Level-0 (add greet and bye)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+
+    private static void levelZero() {
+        System.out.println("Hello! I'm Duke!");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------------");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("");
+        System.out.println("---------------------");
+        System.out.println("");
+
+        levelZero();
+
     }
 }
