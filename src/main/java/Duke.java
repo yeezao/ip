968c42d9f2ee03aa1f451778448a193b2f16f188
yeezao <liangyuzhao1999@gmail.com>
2021-08-22 16:56:32 +0800

    add user input echo and bye exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,45 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private static void levelZero() {
+    private static void firstGreet() {
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
         System.out.println("-------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-------------------------------------");
+    }
+
+    private static void userCommands() {
+        boolean continueProgram = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (continueProgram) {
+            System.out.print("Enter your command here: ");
+            String input = scanner.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println("You entered: " + input);
+            } else {
+                continueProgram = false;
+            }
+        }
     }
 
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        System.out.println("");
         System.out.println("---------------------");
-        System.out.println("");
 
-        levelZero();
+        firstGreet();
+
+        userCommands();
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-------------------------------------");
+
 
     }
 }
